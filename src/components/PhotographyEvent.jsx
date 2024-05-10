b802ccc9cb470408c50acc7fc544c0eef0e1504c
unknown <kalakritievents07@gmail.com>
2024-05-10 23:05:49 +0530

    season 10 dates 20 may

--- src/components/PhotographyEvent.jsx
+++ src/components/PhotographyEvent.jsx
@@ -154,19 +154,19 @@ const ArtEvent = () =>{
         <div className={styles.datesContainer}>
           <div className={styles.dateBox}>
             <p className={styles.dateTitle}>Last Date To Apply</p>
-            <p className={styles.date}>10th May 2024</p>
+            <p className={styles.date}>20th May 2024</p>
           </div>
           <div className={styles.dateBox}>
             <p className={styles.dateTitle}>Registration Start Date</p>
-            <p className={styles.date}>25th April 2024</p>
+            <p className={styles.date}>1st May 2024</p>
           </div>
           <div className={styles.dateBox}>
             <p className={styles.dateTitle}>Result Date</p>
-            <p className={styles.date}>12th May 2024</p>
+            <p className={styles.date}>23rd May 2024</p>
           </div>
           <div className={styles.dateBox}>
             <p className={styles.dateTitle}>Prize Distribution</p>
-            <p className={styles.date}>13th May 2024</p>
+            <p className={styles.date}>25th May 2024</p>
           </div>
         </div>
       </div>
