6b2a5b7340a749ad31ea7699bb121f605551f359
kalakritievents <147000884+kalakritievents@users.noreply.github.com>
2024-07-04 14:16:42 +0530

    Update Result.jsx

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,12 +5,12 @@ import { Card, Dropdown, DropdownButton } from 'react-bootstrap'; // Ensure Reac
 import styles from './ResultPage.module.css'; // Path to your CSS file for this component
 
 const events = [
-    { name: 'Art Event', basepath: '/art', seasons: 10 },
-    { name: 'Mehandi Event', basepath: '/mehandi', seasons: 10 },
-    { name: 'Rangoli Event', basepath: '/rangoli', seasons: 10 },
-    { name: 'Photography Event', basepath: '/photography', seasons: 10 },
-    { name: 'Dance Event', basepath: '/dance', seasons: 4 },
-    { name: 'Singing Event', basepath: '/singing', seasons: 2 },
+    { name: 'Art Event', basepath: '/art', seasons: 11 },
+    { name: 'Mehandi Event', basepath: '/mehandi', seasons: 11 },
+    { name: 'Rangoli Event', basepath: '/rangoli', seasons: 11 },
+    { name: 'Photography Event', basepath: '/photography', seasons: 11 },
+    { name: 'Dance Event', basepath: '/dance', seasons: 5 },
+    { name: 'Singing Event', basepath: '/singing', seasons: 3 },
 ];
 
 const ResultPage = () => {
