a16307402bb37e399dba61b855b6f8b30ebff1b9
kalakritievents <147000884+kalakritievents@users.noreply.github.com>
2024-12-31 10:20:50 +0530

    Update DatesCard.jsx

--- src/components/DatesCard.jsx
+++ src/components/DatesCard.jsx
@@ -12,19 +12,19 @@ const DatesCard = () => {
       <div className={styles.datesContainer}>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Last Date To Apply</p>
-          <p className={styles.date}>15th December 2024</p>
+          <p className={styles.date}>15th January 2025</p>
         </div>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Registration Start Date</p>
-          <p className={styles.date}>01st December 2024</p>
+          <p className={styles.date}>01st January 2025</p>
         </div>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Result Date</p>
-          <p className={styles.date}>16th December 2024</p>
+          <p className={styles.date}>16th January 2025</p>
         </div>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Prize Distribution</p>
-          <p className={styles.date}>19th December 2024</p>
+          <p className={styles.date}>19th January 2025</p>
         </div>
       </div>
     </div>
