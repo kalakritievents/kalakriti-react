1b9d40e4727c6bc51e0d6f781ec53a6e0077650b
kalakritievents <147000884+kalakritievents@users.noreply.github.com>
2025-02-23 14:15:17 +0530

    Update DatesCard.jsx

--- src/components/DatesCard.jsx
+++ src/components/DatesCard.jsx
@@ -5,26 +5,26 @@ import styles from './ArtEvent.module.css';
 const DatesCard = () => {
   return (
     <div className={styles.datesSection}>
-      <h1 className={styles.mainTitle}>GRAND SEASON 15</h1>
+      <h1 className={styles.mainTitle}>GRAND SEASON 16</h1>
       <h2 className={styles.sectionTitle}>IMPORTANT DATES 
 {/*         <span className={styles.updated}>(Updated)</span> */}
       </h2>
       <div className={styles.datesContainer}>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Last Date To Apply</p>
-          <p className={styles.date}>15th January 2025</p>
+          <p className={styles.date}>5th March 2025</p>
         </div>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Registration Start Date</p>
-          <p className={styles.date}>01st January 2025</p>
+          <p className={styles.date}>01st February 2025</p>
         </div>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Result Date</p>
-          <p className={styles.date}>16th January 2025</p>
+          <p className={styles.date}>7th March 2025</p>
         </div>
         <div className={styles.dateBox}>
           <p className={styles.dateTitle}>Prize Distribution</p>
-          <p className={styles.date}>19th January 2025</p>
+          <p className={styles.date}>9th March 2025</p>
         </div>
       </div>
     </div>
